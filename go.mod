2f776aa7e92a1e9661233a9c9e898f3a88fefbdb
pascal <pascalneubeck@gmx.de>
2025-04-12 15:23:51 +0200

    mod init and initial vehicle struct

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/pneubeck/DVT
+
+go 1.24.2
---
+++ structs/vehicle.go
@@ -0,0 +1,7 @@
+package structs
+
+type vehicle struct {
+	ID           int    `json:"id"`
+	VehicleNo    string `json:"vehicleNo"`
+	VehicleColor string `json:"vehicleColor"`
+}

cdcd50cf17c8a19b72676ff31677715a1daf6bf3
pascal <pascalneubeck@gmx.de>
2025-04-12 20:17:52 +0200

    introduced a vehicleController.go file and moved endpoint code there

--- main.go
+++ main.go
@@ -1,26 +1,13 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
-	"github.com/pneubeck/DVT/structs"
+	"github.com/pneubeck/DVT/controller"
 )
-
-// declaration of test vehicle data
-var vehicles = []structs.Vehicle{
-	{ID: 1, VehicleNo: "54365453242345", VehicleColor: "Blue"},
-	{ID: 2, VehicleNo: "63243243432662", VehicleColor: "Yellow"},
-	{ID: 3, VehicleNo: "43435766321454", VehicleColor: "Green"},
-}
 
 func main() {
 	router := gin.Default()
-	router.GET("/vehicles", getVehicles)
+	router.GET("/vehicles", controller.GetVehicles)
 
 	router.Run("localhost:8080")
-}
-
-func getVehicles(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, vehicles)
 }

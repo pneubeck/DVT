4b8fe409a8cc66a6959bdda41223d7b84019007e
pascal <pascalneubeck@gmx.de>
2025-04-12 20:05:53 +0200

    added gin framework and implemented first test GET endpoint

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 	"github.com/pneubeck/DVT/structs"
 )
 
@@ -9,4 +12,15 @@ var vehicles = []structs.Vehicle{
 	{ID: 1, VehicleNo: "54365453242345", VehicleColor: "Blue"},
 	{ID: 2, VehicleNo: "63243243432662", VehicleColor: "Yellow"},
 	{ID: 3, VehicleNo: "43435766321454", VehicleColor: "Green"},
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/vehicles", getVehicles)
+
+	router.Run("localhost:8080")
+}
+
+func getVehicles(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, vehicles)
 }
